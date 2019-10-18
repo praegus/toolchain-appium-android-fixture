154091e69507c329c2ee8881c02da706192dc62a
Johan Roeleveld <johan.roeleveld@praegus.nl>
2019-10-18 13:36:33 +0200

    Added basic swipe functionality

--- src/main/java/nl/praegus/fitnesse/slim/fixtures/AndroidTest.java
+++ src/main/java/nl/praegus/fitnesse/slim/fixtures/AndroidTest.java
@@ -4,6 +4,7 @@ import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.android.nativekey.KeyEvent;
+import io.appium.java_client.touch.offset.PointOption;
 import nl.hsac.fitnesse.fixture.slim.web.annotation.WaitUntil;
 import nl.praegus.fitnesse.slim.util.KeyMapping;
 import org.openqa.selenium.WebElement;
@@ -51,5 +52,23 @@ public class AndroidTest extends AppiumTest<AndroidElement, AndroidDriver<Androi
 
     protected boolean tapElement(TouchAction touchAction, WebElement element) {
         return doIfInteractable(element, () -> touchAction.tap(tapOptions().withElement(element(element))).perform());
+    }
+
+    @WaitUntil
+    public boolean swipeFromTo(String from, String to) {
+        PointOption pointFrom = getPointFromString(from);
+        PointOption pointTo = getPointFromString(to);
+        return swipe(pointFrom, pointTo);
+    }
+
+    protected PointOption getPointFromString(String coordinates) {
+        String[] coordinatesList = coordinates.split(",");
+        return PointOption.point(Integer.parseInt(coordinatesList[0]), Integer.parseInt(coordinatesList[1]));
+    }
+
+    protected boolean swipe(PointOption from, PointOption to) {
+        TouchAction action = new TouchAction(getAppiumHelper().driver());
+        action.longPress(from).moveTo(to).release().perform();
+        return true;
     }
 }
